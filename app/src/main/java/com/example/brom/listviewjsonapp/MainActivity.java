ced6235a75dacc51a5e25c79672839a22dbf113b
a17celal <celinefridalrud@hotmail.com>
2018-04-19 10:31:49 +0200

    JSONobject name, size, ID, cost, type, location. Detta loggas börjat skapa nytt mountainobjekt, detta är färdigt nu och bergen visas i listan meny från grunden

--- app/src/main/java/com/example/brom/listviewjsonapp/MainActivity.java
+++ app/src/main/java/com/example/brom/listviewjsonapp/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -49,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         new FetchData().execute();
         for (int i=0; i<mountainNames.length;i++){
            Mountains m = new Mountains(mountainNames[i],mountainLocations[i], mountainHeights[i]);namnen.add(m);
@@ -70,6 +75,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         myListView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private class FetchData extends AsyncTask<Void,Void,String>{
